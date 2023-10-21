1f246e0da20c4fd405acb120d54675e97a578ff4
tta1214 <tuantran.sw@gmail.com>
2023-10-22 02:46:13 +0700

    update

--- js/ivitation-card.js
+++ js/ivitation-card.js
@@ -91,19 +91,19 @@ back.onclick = function(){
     ivitationCardModal.classList.remove("is-active");
   }
 
-//   document.querySelector(".chucmung_toggle2").addEventListener("click", function(event) {
-//     event.preventDefault();
-//     document.querySelector("#cm-bride").classList.toggle("_chucmung_active");
-//     document.querySelector("#btnMusic").style.opacity = 0;
-//     document.querySelector("#btnMusic").style.visibility = "hidden";
-// });
+  document.querySelector(".click_thamgia").addEventListener("click", function(event) {
+    event.preventDefault();
+    document.querySelector("#cm-join").classList.add("_chucmung_active");
+    document.querySelector("#btnMusic").style.opacity = 0;
+    document.querySelector("#btnMusic").style.visibility = "hidden";
+});
 
-// document.querySelector("._chucmung .close2").addEventListener("click", function(event) {
-//     event.preventDefault();
-//     document.querySelector("#cm-bride").classList.remove("_chucmung_active");
-//     document.querySelector("#btnMusic").style.opacity = 1;
-//     document.querySelector("#btnMusic").style.visibility = "visible";
-// });
+document.querySelector("._thamgia .close").addEventListener("click", function(event) {
+    event.preventDefault();
+    document.querySelector("#cm-join").classList.remove("_chucmung_active");
+    document.querySelector("#btnMusic").style.opacity = 1;
+    document.querySelector("#btnMusic").style.visibility = "visible";
+});
 
 document.querySelector(".chucmung_toggle").addEventListener("click", function(event) {
     event.preventDefault();
