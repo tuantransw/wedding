3e1e70b156ab9b802d5804b7e07bbde49c45823d
tta1214 <tuantran.sw@gmail.com>
2023-10-22 02:21:51 +0700

    update

--- js/ivitation-card.js
+++ js/ivitation-card.js
@@ -108,13 +108,14 @@ back.onclick = function(){
 document.querySelector(".chucmung_toggle").addEventListener("click", function(event) {
     event.preventDefault();
     document.querySelector("#cm-groom").classList.add("_chucmung_active");
-    // document.querySelector("#btnMusic").style.opacity = 0;
-    // document.querySelector("#btnMusic").style.visibility = "hidden";
+    document.querySelector("#btnMusic").style.opacity = 0;
+    document.querySelector("#btnMusic").style.visibility = "hidden";
 });
 
 document.querySelector("._chucmung .close").addEventListener("click", function(event) {
     event.preventDefault();
     document.querySelector("#cm-groom").classList.remove("_chucmung_active");
-    // document.querySelector("#btnMusic").style.opacity = 1;
-    // document.querySelector("#btnMusic").style.visibility = "visible";
+    document.querySelector("#btnMusic").style.opacity = 1;
+    document.querySelector("#btnMusic").style.visibility = "visible";
+    ivitationCardModal.classList.add("is-active");
 });
